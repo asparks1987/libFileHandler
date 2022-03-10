601fa1a3f33a6459c8ff525b54cff63de7f80aaa
Aryn M. Sparks <aryn.sparks1987@gmail.com>
2022-03-09 22:20:02 -0600

    Restructure for github migration

---
+++ libFileHandler.cpp
@@ -0,0 +1,5 @@
+#include "libFileHandler"
+
+silverFile::silverFile()
+{
+}
---
+++ libfilehandler.h
@@ -0,0 +1,12 @@
+#ifndef LIBFILEHANDLER_H
+#define LIBFILEHANDLER_H
+
+#include "libFileHandler_global.h"
+
+class LIBFILEHANDLER_EXPORT LibFileHandler
+{
+public:
+    LibFileHandler();
+};
+
+#endif // LIBFILEHANDLER_H
